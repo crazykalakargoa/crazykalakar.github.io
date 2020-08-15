4b143e64e040159cb228992691a79baad216b0ea
crazykalakargoa <68583469+crazykalakargoa@users.noreply.github.com>
2020-08-15 16:28:08 +0530

    shortlist

--- assets/js/main.js
+++ assets/js/main.js
@@ -174,4 +174,12 @@
     items: 1
   });
 
+
+  // for the table
+  // '.tbl-content' consumed little space for vertical scrollbar, scrollbar width depend on browser/os/platfrom. Here calculate the scollbar width .
+  $(window).on("load resize ", function() {
+    var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
+    $('.tbl-header').css({'padding-right':scrollWidth});
+  }).resize();
+
 })(jQuery);
